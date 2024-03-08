82fa5608106c1c630fa7f0a33cbb058a6022452f
Joe <kavvy99@naver.com>
2024-03-08 11:32:33 +0900

    Chore: 개행 삭제 및 기본 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
 const App = () => {
   return (
     <BrowserRouter>
---
+++ src/style/ImgStyle.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Img = styled.img`
+  width: ${ props => props.width || "100%" };
+  height: ${ props => props.height || "100%" };
+  margin: ${ props => props.margin || "0" };
+  padding: ${ props => props.padding || "0" };
+`;
