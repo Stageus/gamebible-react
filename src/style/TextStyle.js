c86f768bb5c9921dde10da46e30c9732075d7166
조경은 <kavvy99@naver.com>
2024-03-11 13:55:32 +0900

    Style: TextStyle 수정

--- src/style/TextStyle.js
+++ src/style/TextStyle.js
@@ -34,15 +34,15 @@ const setWeight = (value) => {
 
 const template = (props) => {
     return `
-        $width: ${ props.width || "fit-content" }
-        $height: ${ props.height || "fit-content" }
-        ${ FlexStyle(props.flex || "h_center") }
-        $padding: ${ props.padding || "0" }
-        $margin: ${ props.margin || "0" }
-        $color: ${ setColor(props.color || "black") }
-        $background-color: ${ setColor(props.bgColor || "none") }
-        $font-size: ${ props => setSize(props.fontSize || "medium") }
-        $font-weight: ${ setWeight(props.fontWeight || "normal") }
+        width: ${props.$width || "fit-content"}
+        height: ${props.$height || "fit-content"}
+        ${FlexStyle(props.$flex || "h_center_center")}
+        padding: ${props.$padding || "0"}
+        margin: ${props.$margin || "0"}
+        color: ${setColor(props.$color || "black")}
+        background-color: ${setColor(props.$bgColor || "none")}
+        font-size: ${props => setSize(props.vfontSize || "medium")}
+        font-weight: ${setWeight(props.$fontWeight || "normal")}
     `
 }
 
@@ -59,20 +59,16 @@ export const Span = styled.span`
 export const H1 = styled.h1`
     ${props => template(props)}
     font-size: 32px
-    $font-weight: ${ props => setWeight(props.fontWeight || "bold") }
 `
 export const H2 = styled.h2`
     ${props => template(props)}
     font-size: 28px
-    $font-weight: ${ props => setWeight(props.fontWeight || "bold") }
 `
 export const H3 = styled.h3`
     ${props => template(props)}
     font-size: 24px
-    $font-weight: ${ props => setWeight(props.fontWeight || "bold") }
-`     
+`
 export const H4 = styled.h4`
     ${props => template(props)}
     font-size: 22px
-    $font-weight: ${ props => setWeight(props.fontWeight || "bold") }
 `
