8a29a6ee9a57721142d9af454249cf5faacc0931
조경은 <kavvy99@naver.com>
2024-03-12 16:26:56 +0900

    Feat: gameBtnHoverEvent 추가

--- src/component/GameListItem.js
+++ src/component/GameListItem.js
@@ -3,6 +3,7 @@ import { styled } from "styled-components";
 import { Div } from "../style/LayoutStyle";
 import { P } from "../style/TextStyle";
 import { setColor } from "../style/SetStyle";
+import useHover from "../hook/useHover";
 
 const GameListItemContainer = styled(Div)`
   border-bottom-width: 1px;
@@ -19,9 +20,34 @@ const GameListItemName = styled(P)`
 const GameListItem = (props) => {
   const { name } = props.data;
 
+  // ------------------- 호버 이벤트 -------------------
+  const [isHovered, gameBtnHoverEvent, gameBtnHoverOutEvent] = useHover(false);
+
+  const btnStyle = {
+    backgroundColor: isHovered ? `${setColor("minorLight")}` : "transparent",
+  };
+  const nameStyle = {
+    color: isHovered ? `${setColor("white")}` : "black",
+  };
+
   return (
-    <GameListItemContainer $flex="h_center_center" $width="250px" $height="66px" $padding="10px">
-      <GameListItemName $flex="h_start_center" $width="100%" $height="100%">
+    <GameListItemContainer
+      $flex="h_center_center"
+      $width="250px"
+      $height="66px"
+      $padding="10px"
+      onMouseOver={gameBtnHoverEvent}
+      onMouseOut={gameBtnHoverOutEvent}
+      style={btnStyle}
+    >
+      <GameListItemName
+        $flex="h_start_center"
+        $width="100%"
+        $height="100%"
+        onMouseOver={gameBtnHoverEvent}
+        onMouseOut={gameBtnHoverOutEvent}
+        style={nameStyle}
+      >
         {name}
       </GameListItemName>
     </GameListItemContainer>
