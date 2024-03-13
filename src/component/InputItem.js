068dcf38d5bad15cc4c74ecd3843c11aec67273f
Junyeon <jun981008@naver.com>
2024-03-13 15:42:48 +0900

    Feat: props:button 유무에 따라 버튼 출력 기능 추가

--- src/component/InputItem.js
+++ src/component/InputItem.js
@@ -9,16 +9,24 @@ import { Input } from "../style/InputStyle";
 import { Img } from "../style/ImgStyle";
 import { Button } from "../style/ButtonStyle";
 
-const InputDiv = styled(Div)`
+const RelativeDiv = styled(Div)`
   position: relative;
 `;
-const BorderStyleInput = styled(Input)`
+const AbsoluteBtn = styled(Button)`
+  position: absolute;
+  right: 3%;
+  top: 50%;
+  transform: translate(0, -50%);
+  border-style: none;
   border-radius: 8px;
+`;
+const BorderStyleInput = styled(Input)`
+  border-radius: 4px;
 `;
 
 const EyeIconBtn = styled(Button)`
   position: absolute;
-  right: 0;
+  right: 3%;
   top: 50%;
   transform: translate(0, -50%);
   border-style: none;
@@ -39,41 +47,72 @@ const InputItem = (props) => {
     id: {
       key: "id",
       label: "아이디",
-      text: "아이디",
-      constraint: "4 ~ 20글자 제한",
+      placeholder: "4 ~ 20글자 제한",
+      button: "중복확인",
+    },
+    id2: {
+      key: "id2",
+      placeholder: "아이디",
     },
     pw: {
       key: "pw",
       label: "비밀번호",
-      text: "비밀번호",
-      constraint: "8 ~ 20글자 제한",
+      placeholder: "비밀번호",
+    },
+    pw2: {
+      key: "pw2",
+      label: "비밀번호",
+      placeholder: "8 ~ 20글자 제한",
+    },
+    nickName: {
+      key: "nickName",
+      label: "닉네임",
+      placeholder: "2 ~ 20글자 제한",
+      button: "중복확인",
+    },
+    email: {
+      key: "email",
+      label: "이메일",
+      button: "인증전송",
+    },
+    verificationCode: {
+      key: "verificationCode",
+      label: "인증번호",
+      button: "인증확인",
     },
   };
   return (
-    <Div $width="100vw" $height="100vh" $flex="v_center_center">
+    <>
       {Object.values(dummyData).map((data) => (
         <Div key={data.key} $flex="v_start_start" $margin="0 0 20px 0">
-          {data.label !== null ? <Label htmlFor={data.key}>{data.label}</Label> : null}
-          <InputDiv>
-            <BorderStyleInput
-              id={data.key}
-              $width="400px"
-              $height="40px"
-              $padding="0 15px"
-              type={data.key === "pw" ? "password" : "text"}
-              placeholder={data.text}
-            />
-            {data.key === "pw" ? (
+          {data.label && <Label htmlFor={data.key}>{data.label}</Label>}
+          <RelativeDiv>
+            <RelativeDiv>
+              <BorderStyleInput
+                id={data.key}
+                $width="350px"
+                $height="50px"
+                $padding="0 3%"
+                type={data.key === "pw" ? "password" : "text"}
+                placeholder={data.placeholder}
+              />
+              {data.button && (
+                <AbsoluteBtn $color="white" $fontSize="smail" $padding="6px 8px">
+                  {data.button}
+                </AbsoluteBtn>
+              )}
+            </RelativeDiv>
+            {data.key === "pw" && (
               <EyeIconBtn onClick={toggleClickEvent}>
-                <Div $height="24px" $margin="0 10px 0 0">
+                <Div $height="24px">
                   {!toggle ? <Img src={VisibleIcon} /> : <Img src={UnVisibleIcon} />}
                 </Div>
               </EyeIconBtn>
-            ) : null}
-          </InputDiv>
+            )}
+          </RelativeDiv>
         </Div>
       ))}
-    </Div>
+    </>
   );
 };
 
