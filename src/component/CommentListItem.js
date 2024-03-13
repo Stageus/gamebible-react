84b76946aab85047373fb99088d5f1ef39dc02c7
조경은 <kavvy99@naver.com>
2024-03-13 10:43:30 +0900

    Test: 관리자 및 로그인한 사용자의 경우 댓글 삭제 가능 기능 테스트

--- src/component/CommentListItem.js
+++ src/component/CommentListItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { styled } from "styled-components";
 import { setColor } from "../style/SetStyle";
 import { Article, Div } from "../style/LayoutStyle";
+import deleteImg from "../img/deleteImg.svg";
 
 const CommentListItemLayout = styled(Article)`
   background-color: ${setColor("white")};
@@ -11,10 +12,29 @@ const CommentListItemLayout = styled(Article)`
   border-radius: 5px;
   cursor: pointer;
 `;
-const CommentContents = styled(Div)``;
-const CommentTitle = styled(Div)``;
-const CommentNickname = styled(Div)``;
-const CommentCreatedAt = styled(Div)``;
+const CommentContents = styled(Div)`
+  border: 1px solid red;
+`;
+const CommentTitle = styled(Div)`
+  border: 1px solid blue;
+`;
+const CommentNickname = styled(Div)`
+  border: 1px solid blue;
+`;
+const CommentCreatedAt = styled(Div)`
+  border: 1px solid blue;
+`;
+const CommentDelete = styled(Div)`
+  border: 1px solid blue;
+`;
+
+const users = {
+  id: ["admin", "작성자닉네임_2"],
+};
+
+if (users.id.includes("admin") || users.id.includes("작성자닉네임_2")) {
+  console.log("댓글 삭제 가능함");
+}
 
 const CommentListItem = (props) => {
   const { title, nickname, createdAt } = props.data;
@@ -37,6 +57,9 @@ const CommentListItem = (props) => {
         <CommentCreatedAt $flex="h_center_center" $width="100px">
           {createdAt}
         </CommentCreatedAt>
+        <CommentDelete $flex="h_center_center" $width="30px" $height="30px">
+          {users.id.includes(nickname) && <img src={deleteImg} alt="댓글 삭제하기" />}
+        </CommentDelete>
       </CommentContents>
     </CommentListItemLayout>
   );
