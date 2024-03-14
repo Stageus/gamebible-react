2c26f5c83112233f87b7ab971d3443997273537d
조경은 <kavvy99@naver.com>
2024-03-14 13:00:12 +0900

    Test: useRef를 이용한 이미지 업로드 기능 콘솔 내 테스트

--- src/component/AddPhotoBtnItem.js
+++ src/component/AddPhotoBtnItem.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { styled } from "styled-components";
 import { setColor } from "../style/SetStyle";
-import addPhotoImg from "../img/addPhotoImg.svg";
 import { Button } from "../style/ButtonStyle";
+import { Input } from "../style/InputStyle";
 import { Div } from "../style/LayoutStyle";
+import { Img } from "../style/ImgStyle";
+import addPhotoImg from "../img/addPhotoImg.svg";
 
 const AddPhotoBtnItemLayout = styled(Button)`
   border: 1px solid ${setColor("black")};
@@ -11,18 +13,41 @@ const AddPhotoBtnItemLayout = styled(Button)`
 `;
 
 const AddPhotoBtnItem = () => {
+  const fileInput = React.useRef(null);
+  console.log(fileInput);
+
+  const handleButtonClick = () => {
+    fileInput.current.click();
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.files[0]);
+  };
+
   return (
-    <AddPhotoBtnItemLayout
-      $flex="h_center_center"
-      $backgroundColor="white"
-      $padding="15px 20px"
-      $fontWeight="bold"
-    >
-      <Div $margin="0 10px 0 0" $height="24px">
-        <img src={addPhotoImg} alt="사진 첨부하기" />
-      </Div>
-      PHOTO
-    </AddPhotoBtnItemLayout>
+    <>
+      <AddPhotoBtnItemLayout
+        $flex="h_center_center"
+        $backgroundColor="initial"
+        $color="black"
+        $padding="12px 20px"
+        $fontWeight="bold"
+        onClick={handleButtonClick}
+      >
+        <Div $margin="0 10px 0 0" $height="24px">
+          <Img src={addPhotoImg} alt="사진 업로드하기" />
+        </Div>
+        PHOTO
+      </AddPhotoBtnItemLayout>
+
+      <Input
+        type="file"
+        accept="image/*"
+        style={{ display: "none" }}
+        ref={fileInput}
+        onChange={handleChange}
+      />
+    </>
   );
 };
 
