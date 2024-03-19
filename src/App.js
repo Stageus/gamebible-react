55a020b9af59477686dcfe8885e215c8fa5a48ab
junyeon <97601768+hizz1008@users.noreply.github.com>
2024-03-19 16:48:11 +0900

    Feature/page/find id page (#20)
    * Feat: 회원가입 페이지 초기 세팅
    
    * Feat: InputItem 컴포넌트 더미데이터 구현
    
    * Feat: FindIDPage  스타일 구현
    
    * Style: 스타일 수정

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TestPage from "./page/TestPage";
 import GlobalStyle from "./style/GlobalStyle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import FindIDPage from "./page/FindIDPage";
 import WikiPage from "./page/WikiPage";
 import NotificationPage from "./page/NotificationPage";
 
@@ -14,6 +15,7 @@ const App = () => {
           <GlobalStyle />
           <Routes>
             <Route path="/" element={<TestPage />} />
+            <Route path="/FindIDPage" element={<FindIDPage />} />
             <Route path="/wiki" element={<WikiPage />} />
             <Route path="/alarm" element={<NotificationPage />} />
           </Routes>
