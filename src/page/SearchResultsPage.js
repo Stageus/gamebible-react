536cb0f7d807663807c98311fb6ffab92e731ff1
조경은 <kavvy99@naver.com>
2024-03-22 14:26:52 +0900

    Feat: SearchResultsPage 레이아웃 생성

--- src/page/SearchResultsPage.js
+++ src/page/SearchResultsPage.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { styled } from "styled-components";
 import { Div, Section, Article } from "../style/LayoutStyle";
+import { setSize } from "../style/SetStyle";
 
 import HeaderItem from "../component/HeaderItem";
 import GameListContainer from "../container/GameListNavContainer";
@@ -14,6 +15,10 @@ const PageWrapper = styled(Div)`
   min-height: 100vh;
   position: relative;
 `;
+const ArticleLabel = styled(Div)`
+  font-size: ${setSize("large")};
+`;
+const ArticleContentLayout = styled(Div)``;
 const FooterWrapper = styled(Div)`
   position: absolute;
   bottom: 0;
@@ -24,7 +29,7 @@ const SearchResultsPage = () => {
     <PageWrapper>
       <HeaderItem />
       <GameListContainer />
-      <Section $flex="v_center_center" $margin="100px 0" $padding="0 60px" $width="100vw">
+      <Section $flex="v_center_center" $margin="100px 0 0 0" $padding="0 60px" $width="100vw">
         <Article $flex="v_center_center" $margin="300px 0">
           <img src={noResultImg} alt="검색결과가 없습니다" />
           <Div $padding="30px 0">
@@ -35,6 +40,44 @@ const SearchResultsPage = () => {
               backgroundColor="major"
             />
           </Div>
+        </Article>
+      </Section>
+      <Section
+        $flex="v_center_center"
+        $margin="100px 30px 0 30px"
+        $padding="30px"
+        $width="100vw"
+        $backgroundColor="major"
+      >
+        <Article $flex="v_end_" $width="100%" $margin="0 0 30px 0">
+          <ArticleLabel
+            $flex="h_center_center"
+            $backgroundColor="white"
+            $width="150px"
+            $height="50px"
+          >
+            연관 게임
+          </ArticleLabel>
+          <ArticleContentLayout
+            $width="100%"
+            $height="556px"
+            $backgroundColor="white"
+          ></ArticleContentLayout>
+        </Article>
+        <Article $flex="v_end_" $width="100%">
+          <ArticleLabel
+            $flex="h_center_center"
+            $backgroundColor="white"
+            $width="150px"
+            $height="50px"
+          >
+            연관 게시글
+          </ArticleLabel>
+          <ArticleContentLayout
+            $width="100%"
+            $height="556px"
+            $backgroundColor="white"
+          ></ArticleContentLayout>
         </Article>
       </Section>
       <FooterWrapper>
