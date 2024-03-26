c1fed0e85d076fb935c0b9a732395c2e60222496
Joe <kavvy99@naver.com>
2024-03-26 19:14:04 +0900

    YesResultContainer 내 YesGameContainer 생성 및 navToggle 시 SearchResultsPage 사이즈 변경 구현 (#40)
    * 연관 게임 있을 경우 YesResultContainer 내 YesGameContainer 제작 및 navToggle 시 YesResultSection 사이즈 변경 기능 구현
    
    * Style: SearchResultsPage 최종 정리
    
    * Fix: GameListNavContainer 우측 그림자 추가 및 margin 조정을 통한 위치 변경

--- src/page/SearchResultsPage.js
+++ src/page/SearchResultsPage.js
@@ -9,22 +9,23 @@ import FooterItem from "../component/FooterItem";
 import NoResultNoGameContainer from "../container/inSearchResultsPage/NoResultNoGameContainer";
 import YesResultContainer from "../container/inSearchResultsPage/YesResultContainer";
 
+import { useRecoilValue } from "recoil";
+import navToggleAtom from "../recoil/navToggleAtom";
+
 const PageWrapper = styled(Div)`
   min-height: 100vh;
   position: relative;
 `;
-const NoResultSection = styled(Section)`
-  // border: 2px solid red;
-`;
-const YesResultSection = styled(Section)`
-  // border: 2px solid red;
-`;
+const NoResultSection = styled(Section)``;
+const YesResultSection = styled(Section)``;
 const FooterWrapper = styled(Div)`
   position: absolute;
   bottom: 0;
 `;
 
 const SearchResultsPage = () => {
+  const navToggle = useRecoilValue(navToggleAtom);
+
   const searchResultData = [];
 
   return (
@@ -44,23 +45,14 @@ const SearchResultsPage = () => {
       ) : (
         <YesResultSection
           $flex="v_center_center"
-          $margin="100px 0 0 0"
+          $margin={navToggle ? "100px 0 0 300px" : "100px 0 0 0"}
           $padding="0 60px"
-          $width="100vw"
+          $width={navToggle ? "80vw" : "100vw"}
           $backgroundColor="major"
         >
           <YesResultContainer />
         </YesResultSection>
       )}
-      {/* <YesResultSection
-        $flex="v_center_center"
-        $margin="100px 0 0 0"
-        $padding="0 60px"
-        $width="100vw"
-        $backgroundColor="major"
-      >
-        <YesResultContainer />
-      </YesResultSection> */}
       <FooterWrapper>
         <FooterItem />
       </FooterWrapper>
