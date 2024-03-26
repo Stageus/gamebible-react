16a02c1623e280e32535b46766fc75cd991c83b1
Junyeon <jun981008@naver.com>
2024-03-26 18:33:22 +0900

    Style: EditPersonalInfoContainer생성 및 페이지 레이아웃 수정

--- src/page/EditPersonalInfoPage.js
+++ src/page/EditPersonalInfoPage.js
@@ -1,72 +1,20 @@
 import React from "react";
 import HeaderItem from "../component/HeaderItem";
-import InputItem from "../component/InputItem";
 import FooterItem from "../component/FooterItem";
+import EditPersonalInfoContainer from "../container/inEditPersonalInfoPage/EditPersonalInfoContainer";
 
-import MainLogo from "../img/HeaderLogo.svg";
-
-import styled from "styled-components";
-import { Button } from "../style/ButtonStyle";
-import { Div, Section } from "../style/LayoutStyle";
-import { Img } from "../style/ImgStyle";
-import { Link } from "react-router-dom";
+import { Section } from "../style/LayoutStyle";
 
 const dummyData = {
   userIdx: "1234",
 };
-
-const dummyEmailData = {
-  email: {
-    key: "email",
-    label: "이메일",
-    placeholder: "qwer@email.com",
-  },
-  nickName: {
-    key: "nickName",
-    label: "닉네임",
-    placeholder: "홍길동",
-  },
-};
-
-const PositionDiv = styled(Div)`
-  position: relative;
-  top: 70px;
-`;
-
-const FullWideLink = styled(Link)`
-  width: 100%;
-`;
 
 const EditPersonalInfoPage = () => {
   return (
     <>
       <HeaderItem {...{ dummyData }} />
       <Section $width="100vw" $height="90vh" $flex="h_center_center">
-        <PositionDiv $flex="v_center_center" $width="350px">
-          <Img $margin="0 0 20px 0" src={MainLogo} alt="MainLogo" />
-          <InputItem {...{ dummyInputData: dummyEmailData }} />
-          <Button
-            $flex="h_center_center"
-            $width="100%"
-            $color="white"
-            $height="50px"
-            $borderRadius="4px"
-            $margin="0 0 20px 0"
-          >
-            저장하기
-          </Button>
-          <FullWideLink to="/PersonalInfoPage">
-            <Button
-              $flex="h_center_center"
-              $width="100%"
-              $color="white"
-              $height="50px"
-              $borderRadius="4px"
-            >
-              취소하기
-            </Button>
-          </FullWideLink>
-        </PositionDiv>
+        <EditPersonalInfoContainer />
       </Section>
       <FooterItem />
     </>
