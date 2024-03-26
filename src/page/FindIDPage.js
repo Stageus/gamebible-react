b3fcfdb690142829f28da4ef3d9ebc6e46248bb9
Junyeon <jun981008@naver.com>
2024-03-26 18:37:12 +0900

    Style: FindIDPage 레이아웃 수정

--- src/page/FindIDPage.js
+++ src/page/FindIDPage.js
@@ -1,57 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import HeaderItem from "../component/HeaderItem";
-import InputItem from "../component/InputItem";
+import FindIDContainer from "../container/inFindIDPage/FindIDContainer";
 import FooterItem from "../component/FooterItem";
-import { emailValueValidation } from "../util/ValidationUtil";
 
-import MainLogo from "../img/HeaderLogo.svg";
-
-import styled from "styled-components";
-import { Button } from "../style/ButtonStyle";
-import { Div, Section } from "../style/LayoutStyle";
-import { Img } from "../style/ImgStyle";
+import { Section } from "../style/LayoutStyle";
 
 const userIdx = "null";
 
-const dummyEmailData = {
-  pw: {
-    key: "email",
-    label: "이메일",
-    placeholder: "가입된 이메일 주소 입력",
-  },
-};
-
-const PositionDiv = styled(Div)`
-  position: relative;
-  top: 70px;
-`;
-
 const FindIDPage = () => {
-  const [emailValue, setEmailValue] = useState("");
-  const findIdClickEvent = () => {
-    if (!emailValueValidation(emailValue)) {
-      return;
-    }
-    console.log("아이디 찾기 API 호출");
-  };
   return (
     <>
       <HeaderItem {...{ userIdx }} />
       <Section $width="100vw" $height="90vh" $flex="h_center_center">
-        <PositionDiv $flex="v_center_center" $width="350px">
-          <Img $margin="0 0 20px 0" src={MainLogo} alt="MainLogo" />
-          <InputItem {...{ dummyInputData: dummyEmailData, inputValue: setEmailValue }} />
-          <Button
-            $flex="h_center_center"
-            $width="100%"
-            $color="white"
-            $height="50px"
-            $borderRadius="4px"
-            onClick={findIdClickEvent}
-          >
-            아이디 찾기
-          </Button>
-        </PositionDiv>
+        <FindIDContainer />
       </Section>
       <FooterItem />
     </>
