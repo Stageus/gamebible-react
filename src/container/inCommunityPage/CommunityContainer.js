b680cc90d5663db72ea19fefbf8949f1de43d77d
Junyeon <jun981008@naver.com>
2024-04-03 14:46:20 +0900

    일단

--- src/container/inCommunityPage/CommunityContainer.js
+++ src/container/inCommunityPage/CommunityContainer.js
@@ -56,31 +56,6 @@ const CommunityContainer = () => {
 
   const navToggle = useRecoilValue(navToggleAtom);
 
-  const { idx, pageIdx } = useParams();
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.REACT_APP_API_KEY}/post?gameidx=${idx}&page=${pageIdx}`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const result = await response.json();
-        setData(result);
-        console.log(result);
-      } catch (error) {
-        console.log(`Error: ${error.message}`);
-      }
-    };
-    fetchData();
-  }, [pageIdx]);
-
   return (
     <GameContentLayout $flex="v_center_center" $padding={navToggle && "0 0 0 250px"}>
       <BannerImgItem />
