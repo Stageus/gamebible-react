155fe98325c11d36b08fa046227993ff7299d7e3
조경은 <kavvy99@naver.com>
2024-04-08 18:06:45 +0900

    알림 목록 보기, 게임 이미지 설정 관련 API 연결 중

--- src/container/inNotificationPage/GameImgSettingContainer.js
+++ src/container/inNotificationPage/GameImgSettingContainer.js
@@ -10,6 +10,8 @@ import { setColor } from "../../style/SetStyle";
 import { Button } from "../../style/ButtonStyle";
 import { Section } from "../../style/LayoutStyle";
 
+import { useCookies } from "react-cookie";
+
 const RelativeDiv = styled(Div)`
   position: relative;
 `;
@@ -29,6 +31,42 @@ const RightButton = styled(Button)`
 
 const GameImgSettingContainer = (props) => {
   const { setGameImgEvent } = props;
+  const cookies = useCookies();
+  const [thumbnailImg, setThumbnailImg] = useState(null);
+  const [bannerImg, setBannerImg] = useState(null);
+
+  const approveGameEvent = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_KEY}/admin/game`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.token}`,
+      },
+      body: {
+        requestIdx: `${requestIdx}`,
+        images: `${thumbnailImg} ${bannerImg}`,
+      },
+    });
+
+    const result = await response.json();
+    if (response.status === 400) {
+      alert(result.message);
+    } else if (response.status === 409) {
+      alert(result.message);
+    } else if (response.status === 500) {
+      alert(result.message);
+    }
+  };
+
+  const handleThumbnailImgChange = (event) => {
+    const thumbanilFile = event.target.files[0];
+    setThumbnailImg(thumbanilFile);
+  };
+  const handleBannerImgChange = (event) => {
+    const bannerFile = event.target.files[0];
+    setThumbnailImg(bannerFile);
+  };
+
   return (
     <RelativeSection $flex="h_center_center" $width="100vw" $height="100vh">
       <RelativeDiv $flex="v_start_center" $width="500px" $height="300px" $backgroundColor="white">
@@ -48,11 +86,11 @@ const GameImgSettingContainer = (props) => {
         <Section $flex="v_between_center" $margin="40px 0">
           <Div $margin="0 0 20px 0">
             <Div>게임 배너 이미지</Div>
-            <Input type="file" />
+            <Input type="file" onChange={handleThumbnailImgChange} />
           </Div>
           <Div $margin="0 0 40px 0">
             <Div>게임 썸네일 이미지</Div>
-            <Input type="file" />
+            <Input type="file" onChange={handleBannerImgChange} />
           </Div>
           <Div $flex="h_between_center" $width="100%">
             <Button
@@ -69,6 +107,7 @@ const GameImgSettingContainer = (props) => {
               $backgroundColor="major"
               $color="white"
               $borderRadius="10px"
+              onClick={approveGameEvent}
             >
               확인
             </Button>
