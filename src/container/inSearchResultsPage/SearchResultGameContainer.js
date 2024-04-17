f19b41d9b9e631c569fb9f6551b51f48fa546496
조경은 <kavvy99@naver.com>
2024-04-17 13:00:09 +0900

    Fix: SearchResultGameContainer 내 예외처리 알람창 내용 수정

--- src/container/inSearchResultsPage/SearchResultGameContainer.js
+++ src/container/inSearchResultsPage/SearchResultGameContainer.js
@@ -41,12 +41,12 @@ const SearchResultGameContainer = () => {
       console.log("게임 검색결과가 없습니다.");
     }
     if (status === 400) {
-      alert("유효하지 않은 요청입니다.");
+      return alert("두 글자 이상 입력하시면 더 좋은 검색결과를 얻으실 수 있습니다!😊");
     }
     if (status === 500) {
       console.log("서버 내부 에러입니다.");
     }
-  }, [data, status]); // status 달아줬어야 함 ( 204는 응답 body가 없어서 반응을 안함 )
+  }, [data, status]); // status 달아줘야함 ( 204는 응답 body가 없어서 반응을 안 하기 때문 )
 
   return (
     <Article $flex="v_center_start" $width={navToggle ? "90%" : "100%"} $margin="30px 0 30px 0">
